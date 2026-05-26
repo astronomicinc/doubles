79d51ee1246f3c782d5dffb24a896bd9faea00e9
Chris Beaman <chris.beaman@gmail.com>
2026-05-25 18:18:24 -0700

    Add test page for routing verification

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{padding: '20px', fontFamily: 'sans-serif'}}>
+      <h1>Test page works!</h1>
+      <p>If you can see this, routing is functioning correctly.</p>
+    </div>
+  );
+}
